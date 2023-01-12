4689c35ad0091ea33631d576d316947582eb066f
NanniesX <57839809+NNCZ12@users.noreply.github.com>
2023-01-12 16:30:21 +0700

    commit

---
+++ .history/frontend/src/components/Maps_20230112134942.jsx
@@ -0,0 +1,67 @@
+import { React, useState, useContext } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import image from "../assets/marker.png";
+import ProfileForm from "./ProfileForm";
+import { Form, FloatingLabel, Button, Modal } from "react-bootstrap";
+
+function Maps(props,{handleGetLatLon}) {
+  let markerIcon = L.icon({
+    iconUrl: image,
+    iconSize: [40, 40],
+  });
+ 
+  const center = [14.875238941094505, 102.01287487655536];
+  const zoom = 15;
+  const GetLatLon = () => {
+    const map = useMapEvents({
+      click(e) {
+        const latlon = e.latlng
+        handleGetLatLon(latlon)
+      },
+    });
+    return position == null ? null : (
+      <Marker icon={markerIcon} position={position}></Marker>
+    );
+  };
+
+  
+
+  return (
+    <div>
+     
+      <div className="p-1 h-[90%]">
+        <MapContainer
+          style={{ height: "80vh" }}
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={false}
+          
+        > <GetLatLon></GetLatLon>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          ></TileLayer>
+         
+        </MapContainer>
+      </div>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.onClick}>
+          Close
+        </Button>
+        <Button variant="success"  onClick={props.onClick}>
+          OK
+        </Button>
+      </Modal.Footer>
+    </div>
+  );
+}
+
+export default Maps;
+// ReactDOM.render(<Maps/>, document.getElementById('maps'));
