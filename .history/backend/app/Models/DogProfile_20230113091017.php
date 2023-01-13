5bd822d56765978a24f66cc9cdab2256a4ebe7bb
NanniesX <57839809+NNCZ12@users.noreply.github.com>
2023-01-13 11:27:33 +0700

    commit

---
+++ .history/backend/app/Models/DogProfile_20230113091017.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class DogProfile extends Model
+{
+    use HasFactory;
+   
+    protected $fillable = ['name','vaccination','vaccine_date','spot_on_date','spot_on','neuter','gender','dog_character','appearance','location_name','color','image','note',''];
+    
+}
---
+++ .history/frontend/src/components/Maps_20230113095820.jsx
@@ -0,0 +1,69 @@
+import { React, useState, useContext } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import image from "../assets/marker.png";
+import ProfileForm from "./ProfileForm";
+import { Form, FloatingLabel, Button, Modal } from "react-bootstrap";
+
+function Maps( {handleCloseMap,handleGetLatLon}) {
+  let markerIcon = L.icon({
+    iconUrl: image,
+    iconSize: [40, 40],
+  });
+  const center = [14.875238941094505, 102.01287487655536];
+  const zoom = 15;
+  const [latlon,setLatlon] =useState(null);
+
+  const HandleGetLatLon = () => {
+    const map = useMapEvents({
+      click(e) {
+        const latlon = e.latlng
+        setLatlon(latlon);
+        handleGetLatLon(latlon.lat, latlon.lng)
+        console.log(latlon);
+      }
+    });
+   return latlon == null? null : <Marker position={latlon} icon={markerIcon}></Marker>
+  };
+
+  
+  return (
+    <div>
+      <div className="p-1 h-[90%]">
+        {latlon == null? null: <><p>{latlon.lat}</p> <p></p></>}
+        
+        <MapContainer
+          style={{ height: "80vh" }}
+          center={center}
+          zoom={zoom}
+          scrollWheelZoom={false}
+          
+        > 
+        <HandleGetLatLon/>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          ></TileLayer>
+         
+        </MapContainer>
+      </div>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleCloseMap}>
+          Close
+        </Button>
+        <Button variant="success"  onClick={()=>{handleCloseMap()}}>
+          OK
+        </Button>
+      </Modal.Footer>
+    </div>
+  );
+}
+
+export default Maps;
+// ReactDOM.render(<Maps/>, document.getElementById('maps'));
